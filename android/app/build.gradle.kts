c742c40b86c4878053414ff602e2160747680a64
SatyamBhargav <satyam.b@codevit.in>
2025-11-06 15:59:20 +0530

    Update application identifiers and labels for consistency across platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.toskie_mobile"
+        applicationId = "com.toskie.toskieMobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
